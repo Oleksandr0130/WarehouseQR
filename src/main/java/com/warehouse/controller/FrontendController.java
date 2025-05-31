91e07b45d60970b19fadc0becd11bf5f312795ff
Oleksandr0130 <olek1995sta@gmail.com>
2025-05-31 16:24:09 +0200

    new

--- src/main/java/com/warehouse/controller/FrontendController.java
+++ src/main/java/com/warehouse/controller/FrontendController.java
@@ -3,13 +3,20 @@ package com.warehouse.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//@Controller
+//public class FrontendController {
+//
+//    @GetMapping("/{path:[^\\.]*}")
+//    public String redirect() {
+//        // Отдает index.html для любого маршрута, который не содержит точку (например, /about)
+//        return "forward:/index.html";
+//    }
+//}
 @Controller
 public class FrontendController {
 
-    @GetMapping("/{path:[^\\.]*}")
+    @GetMapping(value = {"/api", "/api/{path:^(?!api$).*$}", "/api/{path:^(?!api$).*$}/**"})
     public String redirect() {
-        // Отдает index.html для любого маршрута, который не содержит точку (например, /about)
         return "forward:/index.html";
     }
 }
-
