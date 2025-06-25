8c73e4c6896de70051d77c70da6eba1ef50492cd
Oleksandr0130 <olek1995sta@gmail.com>
2025-06-25 18:57:46 +0200

    new

--- src/main/java/com/warehouse/controller/UserController.java
+++ src/main/java/com/warehouse/controller/UserController.java
@@ -1,6 +1,12 @@
 package com.warehouse.controller;
 
+import com.warehouse.model.dto.UserRegistrationDTO;
+import com.warehouse.service.CompanyService;
+import com.warehouse.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class UserController {
+    private final CompanyService companyService; // Сервис компании
 }
